66b78d978737c398d6207d62794f805c81abb3e0
Aminul667 <rahathssn@gmail.com>
2021-07-15 03:01:17 +0600

    add basic concept files

---
+++ 1.1_Data_Type.R
@@ -0,0 +1,22 @@
+# integer
+x <- 3L
+typeof(x)
+
+# double
+y <- 5.5
+typeof(y)
+
+# complex 
+z <- 3 + 2i
+typeof(z)
+
+# character
+a <- "Hello"
+typeof(a)
+
+# logical
+q1 <- T
+typeof(q1)
+
+q2 <- FALSE
+typeof(q2)
---
+++ 1.3_LogicalOperator.R
@@ -0,0 +1,25 @@
+# Logical:
+# TRUE T
+# FALSE F
+
+# ==
+# !=
+# <
+# >
+# <=
+# >=
+# not: !
+# or: |
+# and: &
+# isTRUE(x), a function to check
+
+result1 <- 4 < 5
+result1
+
+result2 <- !(5 > 1)
+result2
+
+result1 | result2
+result1 & result2
+
+isTRUE(result1)
